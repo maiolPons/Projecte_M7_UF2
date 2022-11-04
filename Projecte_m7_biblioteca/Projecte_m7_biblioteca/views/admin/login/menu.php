7d2f1a7f0c081d10406f990358aabfd30b8533e1
maiolPons <100287697+maiolPons@users.noreply.github.com>
2022-11-04 01:12:27 -0700

    Delete Projecte_m7_biblioteca/Projecte_m7_biblioteca directory

--- Projecte_m7_biblioteca/Projecte_m7_biblioteca/views/admin/login/menu.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li><a href="index.php?controller=libro&action=mostrarLibros">Productos</a></li>
-    <li><a href="">categorias</a></li>
-    <li><a href="">Pedidos</a></li>
-    <li><a href="index.php?controller=admin&action=salir">Cerrar sesión</a></li>
-</ul>
--- Projecte_m7_biblioteca/Projecte_m7_biblioteca/views/admin/productos/editFoto.php
+++
@@ -1,9 +0,0 @@
-<h1>Modificar imagen</h1>
-<form action="index.php?controller=libro&action=editarFoto" method="POST" enctype="multipart/form-data">  
-    <div>
-        <label for="archivo">Imagen </label>
-        <input type="file" name="archivo" required/><br>
-        <input type="hidden" name="isbn" value="<?php echo $isbn ?>">
-    </div>
-    <input type="submit" name="Modificar" value="Modificar"/>
-</form> 
