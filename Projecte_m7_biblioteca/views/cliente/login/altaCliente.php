823827243e9e6a9e62cf0b497a22e9da1d195c56
diana120500 <113168737+diana120500@users.noreply.github.com>
2022-11-09 09:23:27 +0100

    Add files via upload

--- Projecte_m7_biblioteca/views/cliente/login/altaCliente.php
+++ Projecte_m7_biblioteca/views/cliente/login/altaCliente.php
@@ -1,11 +1,13 @@
 <div class="formAltaUsu">
-    <h1>Alta Usuario</h1>
-    <form class="loginAltaUsu" action="" method="">
-        <input type="email" name="email" placeholder="E-mail" required> 
-        <input type="text" name="nombre" placeholder="Nombre" required> 
-        <input type="text" name="apellidos" placeholder="Apellidos" required> 
-        <input type="text" name="direccion" placeholder="Dirección" required> 
-        <input type="text" name="dni" placeholder="DNI" required> 
-        <input type="password" name="passwd" placeholder="Contraseña" required> 
+    <h1>Crear nueva cuenta</h1>
+    <form class="loginAltaUsu" action="index.php?controller=cliente&action=crearCliente" method="POST">
+        <input type="email" name="email" placeholder="E-mail*" required> 
+        <input type="text" name="nombre" placeholder="Nombre*" required> 
+        <input type="text" name="apellidos" placeholder="Apellidos*" required> 
+        <input type="text" name="direccion" placeholder="Dirección*" required> 
+        <input type="text" name="dni" placeholder="DNI*" required> 
+        <input type="password" name="passwd" placeholder="Contraseña*" required> 
+        <p>Todos los campos son obligatorios*</p>
+        <input type="submit" name="enviar" value="Crear cuenta"> 
     </form>
 </div>
