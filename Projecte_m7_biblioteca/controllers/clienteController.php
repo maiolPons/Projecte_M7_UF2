c09debe38e26325cd13acd3af75352b07c16c58c
diana120500 <113168737+diana120500@users.noreply.github.com>
2022-11-09 09:22:40 +0100

    Add files via upload

--- Projecte_m7_biblioteca/controllers/clienteController.php
+++ Projecte_m7_biblioteca/controllers/clienteController.php
@@ -34,6 +34,36 @@ class ClienteController{
     //Crear Cuenta
     public function crearCliente(){
         require_once "views/cliente/login/altaCliente.php";
+        if (isset($_POST['enviar'])){
+            $cliente= new Cliente();
+            $cliente->setEmail($_POST['email']);
+            $cliente->setNombre($_POST['nombre']);
+            $cliente->setApellido($_POST['apellidos']);
+            $cliente->setDireccion($_POST['direccion']);
+            $cliente->setDni($_POST['dni']);
+            $cliente->setContrasenya($_POST['passwd']);
+            if ($cliente->comprobarDuplicados()){
+                ?>
+                    <script>alert("Este e-mail o DNI ya han sido registrados.Vuelve a rellenar los campos.")</script>
+                    <META HTTP-EQUIV="REFRESH" CONTENT="0;URL=index.php?controller=cliente&action=crearCliente">
+                <?php
+            }
+            else{
+                if ($cliente->anyadirCliente()==true){
+                    ?>
+                    <script>alert("Usuario creado satisfactoriamente! Ya puedes iniciar sesión")</script>
+                    <META HTTP-EQUIV="REFRESH" CONTENT="0;URL=index.php?controller=cliente&action=logearCliente">
+                    <?php
+                }
+                else{
+                    ?>
+                    <script>alert("Algo no ha ido como esperabamos, por favor vuelve a rellenar los campos")</script>
+                    <META HTTP-EQUIV="REFRESH" CONTENT="0;URL=index.php?controller=cliente&action=crearCliente">
+                    <?php 
+                }
+            }
+        }
+
     }
     
     //Cerrar sesion
