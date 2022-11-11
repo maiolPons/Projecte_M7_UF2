8e6f7355983fff8eec5760c4226ce83404de9e54
diana120500 <113168737+diana120500@users.noreply.github.com>
2022-11-11 08:21:43 +0100

    Add files via upload

---
+++ libreria/views/admin/commonAdmin/headerCliente.php
@@ -0,0 +1,22 @@
+<nav id="menucliente">
+    <ul>
+		<li class="first_child"><a href="#">Libros</a>
+			<?php 
+				foreach($categorias as $categoria){
+				echo "<ul>";
+				echo "<li><a href='#'>".$categoria['nombre']."</a></li>";
+				echo "</ul>";
+				}
+			?>
+		
+		<li class="second-child"><img class="perfil" src="pic/perfil.png" alt="">
+			<ul>
+				<li><a href="index.php?controller=libro&action=detalleLibro">Detalle libro</a></li>
+				<li><a href="#">Modificar perfil</a></li>
+				<li><a href="index.php?controller=libro&action=favoritos">Mis favoritos</a></li>
+				<li><a href="#">Mis pedidos</a></li>
+				<li><a href="index.php?controller=admin&action=salir">Cerrar sesión</a></li>
+			</ul>
+		</li>
+	 </ul>
+</nav>
