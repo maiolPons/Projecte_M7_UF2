65f7b746828ea9436f2db2cd4680fa6fbb40b35f
alsalma1 <113168469+alsalma1@users.noreply.github.com>
2022-11-22 13:11:37 +0100

    Update detalleLibro.php

--- libreria/views/cliente/libro/detalleLibro.php
+++ libreria/views/cliente/libro/detalleLibro.php
@@ -1,4 +1,45 @@
-<?php
+<div class="divMenuVertival">
+    <ul class="menuVertival">
+    <?php
+        if(isset($_SESSION['cliente'])){?>
+
+            <li id="first">
+                <img src="pic/usuario.png" alt="">
+                <a class="profile" href="index.php?controller=cliente&action=miPerfil">Mi perfil</a>
+            </li>
+            <li id="second">
+                <img src="pic/news.png" alt="">
+                <a class="messages" href="index.php">Novedades </a>
+            </li>
+            <li id="third">
+                <img src="pic/corazon.png" alt="">
+                <a class="favoritos" href="index.php?controller=libro&action=favoritos">Mis favoritos</a>
+            </li>
+            <li id="fourth">
+                <img src="pic/sent.png" alt="">
+                <a class="pedidos" href="#">Mis pedidos</a>
+            </li>
+        <?php
+        }
+        else{?>
+            <li id="first">
+                <img src="pic/libr.png" alt="">
+                <a class="messages" href="index.php">Página Principal</a>
+            </li>
+            <li id="second">
+                <img src="pic/news.png" alt="">
+                <a class="favoritos" href="index.php">Novedades</a>
+            </li>
+            <li id="third">
+                <img src="pic/perfil.png" alt="">
+                <a class="favoritos" href="index.php?controller=cliente&action=logearCliente">Iniciar sesión</a>
+            </li>
+            <?php
+        }
+        ?>
+    </ul>
+</div>
+  <?php
     foreach($rows as $row){
         $isbn = $row['ISBN'];
         ?>
@@ -7,16 +48,18 @@
                 <!-- Imagen -->
                 <div class="divimagenLibro">
                     <?php
-                    if($row['favorito']==1){?>
-                        <a href="index.php?controller=libro&action=NoEsFavorito&isbn=<?php echo $isbn; ?>"><img src="pic/corazonRojo.png" alt=""></a>
-                        <?php
-                    }
-                    else{?>
-                        <a href="index.php?controller=libro&action=esFavorito&isbn=<?php echo $isbn; ?>"><img src="pic/corazonNegro.png" alt=""></a>
-                        <?php
+                    if(isset($_SESSION['cliente'])){
+                        if($row['favorito']==1){?>
+                            <a href="index.php?controller=libro&action=NoEsFavorito&isbn=<?php echo $isbn; ?>"><img src="pic/corazonRojo.png" alt=""></a>
+                            <?php
+                        }
+                        else{?>
+                            <a href="index.php?controller=libro&action=esFavorito&isbn=<?php echo $isbn; ?>"><img src="pic/corazonNegro.png" alt=""></a>
+                            <?php
+                        }
                     }
                     ?>
-                    <img  src="<?php echo $row['foto']?>" alt="">
+                    <img src="<?php echo $row['foto']?>" alt="">
                 </div>
             </div>
             <div>
