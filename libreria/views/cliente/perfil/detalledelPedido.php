2b2af00879f50dbe4b2ba8ebdc72f84b416ce57e
diana120500 <113168737+diana120500@users.noreply.github.com>
2022-12-19 10:16:16 +0100

    Add files via upload

--- libreria/views/cliente/perfil/detalledelPedido.php
+++ libreria/views/cliente/perfil/detalledelPedido.php
@@ -14,11 +14,11 @@ for($i=0;$i<count($arrayPedido);$i++){
     for($j=0;$j<count($arrayPedido[1]);$j++){
             if ($i!=0){?>
             <tr>
-                <td><img src="<?php echo $arrayPedido[$i][$j][5]?>"></td>
+                <td><img class="imgDet" src="<?php echo $arrayPedido[$i][$j][5]?>"></td>
                 <td><?php echo $arrayPedido[$i][$j][0]?></td>
                 <td><?php echo $arrayPedido[$i][$j][1]?></td>
                 <td><?php echo $arrayPedido[$i][$j][2]?></td>
-                <td><?php echo $arrayPedido[$i][$j][7]?></td>
+                <td><?php echo $arrayPedido[$i][$j][7]?>€</td>
                 <td><?php echo $arrayPedido[$i][$j][3]?></td>
                 <td><?php echo $arrayPedido[$i][$j][9]?></td>
             </tr> <?php
