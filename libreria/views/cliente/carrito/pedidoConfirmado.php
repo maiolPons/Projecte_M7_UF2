870055fda01c8fb5efd2b34c78f8011b906f6597
maiolPons <100287697+maiolPons@users.noreply.github.com>
2022-12-19 22:18:08 +0100

    Delete libreria directory

--- libreria/views/cliente/carrito/pedidoConfirmado.php
+++
@@ -1,5 +0,0 @@
-<div id="pedidoRealizadoDiv">
-    <p class="pedidoRealizado">Pedido realizado con exito</p>
-    <p class="pedidoRealizado">Gracias por usar nuestros servicios</p>
-    <a href="index.php">Volver a la pagina de inicio</a>
-</div>
--- libreria/views/cliente/libro/esperandoPeticion.php
+++
@@ -1,6 +0,0 @@
-<div id="esperandoPeticion">
-    <img src="pic/Loading_icon.gif">
-    <?php
-        //var_dump($_SESSION);
-    ?>
-</div>
