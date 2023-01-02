2dd4306d93840723d61da38571a4b9f9cabdcc2d
alsalma1 <113168469+alsalma1@users.noreply.github.com>
2023-01-02 22:07:32 +0100

    Add files via upload

--- libreria/index.php
+++ libreria/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="styles/styles.css" rel="stylesheet" type="text/css">
+    <link href="views/styles/styles.css" rel="stylesheet" type="text/css">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="views/admin/pedidos/pedidos.js"></script>
     <script src="views/cliente/libro/scripts.js"></script>
--- libreria/views/cliente/general/resultadoBusqueda.php
+++ libreria/views/cliente/general/resultadoBusqueda.php
@@ -8,7 +8,7 @@
         echo "</div>";
     }else{
         echo "<h3 class='tituloPantallaBuscar'>No hay resultados en la busqueda!</h3>";
-        echo "<h3 class='tituloPantallaBuscar'>Pero quiza te interesa esto!</h3><br>";
+        echo "<h3 class='tituloPantallaBuscar'>Pero quizá te interesa esto!</h3><br>";
         echo "<div class='mostrarProductos'>";
         foreach($rows as $info){
             echo "<a href='index.php?controller=libro&action=detalleLibro&isbn=".$info["ISBN"]."'><div class='displayItem'><img src='".$info["foto"]."'><hr><p>".$info["titulo"]."</p><hr><p>Autor: ".$info["autor"]."</p><p>Editorial: ".$info["editorial"]."</p><p>Precio: ".$info["precioUni"]."€</p></div></a>";
