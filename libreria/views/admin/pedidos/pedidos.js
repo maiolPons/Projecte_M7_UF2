21a9dcce265c323243dfa864c8eb38022dd5e952
maiolPons <100287697+maiolPons@users.noreply.github.com>
2023-01-06 23:31:46 +0100

    Update pedidos.js

--- libreria/views/admin/pedidos/pedidos.js
+++ libreria/views/admin/pedidos/pedidos.js
@@ -15,7 +15,7 @@ function mostrarDetalles(info){
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="styles/styles.css" rel="stylesheet" type="text/css">
+        <link href="views/styles/styles.css" rel="stylesheet" type="text/css">
         <title>Detalles de Pedido</title>
     </head>
     <body>
