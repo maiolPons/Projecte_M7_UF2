a1a1c1412ab1efb8650e7b528cb0f5132fc1f619
Diana <113168737+diana120500@users.noreply.github.com>
2023-01-10 10:03:26 +0100

    Add files via upload

--- libreria/views/cliente/perfil/misPedidos.php
+++ libreria/views/cliente/perfil/misPedidos.php
@@ -29,7 +29,7 @@
     else{
         echo "<h2 style='text-align:center; padding-top:3%;margin-bottom:3%; border-bottom: 2px solid;padding-bottom:1%;'> Mis pedidos </h2>";
         echo "<div class='tablaP0'>";
-            echo "<table>"; ?>
+            echo "<table class='tb'>"; ?>
             <tr>
                 <td>ID PEDIDO  </td>
                 <td>FECHA DE PETICIÓN</td>
@@ -38,14 +38,29 @@
                 <td>DETALLE DEL PEDIDO</td>
             </tr>
         <?php
-        foreach ($pedidos as $pedido) { ?>
+        foreach ($pedidos as $pedido) { 
+            //girar fechas
+            $objeto_DateTime = date_create_from_format('Y-m-d', $pedido['fechaPeticion']);
+            $cadena_nuevo_formato = date_format($objeto_DateTime, "d/m/Y");
+            ?>
+        
             <tr>
                 <?php $id=$pedido['id']?>
                 <td><?php echo $pedido['id']?></td>
-                <td><?php echo $pedido['fechaPeticion']?></td>
-                <td><?php echo $pedido['estado']?></td>
-                <td><?php echo $pedido['ImporteTotal']?></td>
-                <td><a href="index.php?controller=pedido&action=detalledelPedido&id=<?php echo $id ?>">Detalle</a></td>
+                <td><?php echo $cadena_nuevo_formato?></td>
+                <?php 
+                if ($pedido['estado']=='pendiente'){
+                    ?><td><p style="color:orange";><?php echo $pedido['estado']?></p></td><?php
+                }
+                else if($pedido['estado']=='enviado'){
+                    ?><td><p style="color:green";><?php echo $pedido['estado']?></p></td><?php
+                }
+                else{
+                    ?><td><p><?php echo $pedido['estado']?></p></td><?php
+                }
+                ?>
+                <td><?php echo $pedido['ImporteTotal']?>€</td>
+                <td><a id="detalleP" href="index.php?controller=pedido&action=detalledelPedido&id=<?php echo $id ?>">Detalle</a></td>
             </tr> <?php
             
         }
